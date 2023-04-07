2a7069edd661c735da0063cce7f02cfb792a8a79
Combatd <combatdstar@gmail.com>
2023-04-06 17:30:29 -0700

    Create Demo class

---
+++ src/basic/javaProgram.java
@@ -0,0 +1,7 @@
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hi there");
+        System.out.println("This is a demo");
+        System.out.println("Bye");
+    }
+}
