f17b7f1e5df60456047058b32c9a01da4c940c4b
Combatd <combatdstar@gmail.com>
2023-04-06 18:52:49 -0700

    Calculate tournament scores

---
+++ src/basic/chessTournamentScores.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        int wins = 54;
+        int ties = 31;
+
+        double total = ties * 0.5 + wins;
+        System.out.println(total);
+    }
+}
